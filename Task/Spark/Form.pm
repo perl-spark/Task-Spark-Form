822e103e77f721ebed11751c629daa6762d75c08
Kent Fredric <kentfredric@gmail.com>
2009-06-30 00:49:24 +1200

    Initial dummy package

---
+++ Task/Spark/Form.pm
@@ -0,0 +1,24 @@
+package Task::Spark::Form;
+
+# ABSTRACT All you need to start with Spark::Form
+# $Id:$
+use strict;
+use warnings;
+
+=head1 SYNOPSIS
+
+perl -MCPAN -e 'install Task::Spark::Form'
+
+=cut
+
+=head1 DESCRIPTION
+
+The bare essentials of Spark::Form, with everything
+you'll probably ever want to use to do anything with it.
+
+=cut
+
+1;
+
+
+
