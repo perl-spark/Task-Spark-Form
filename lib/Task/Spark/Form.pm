3082d4435301f5aba1eba629cffe3db007de0f9a
Kent Fredric <kentfredric@gmail.com>
2009-07-22 11:04:28 +1200

    postevent 0.01 build

---
+++ lib/Task/Spark/Form.pm
@@ -0,0 +1,49 @@
+package Task::Spark::Form;
+our $VERSION = '0.01';
+
+
+# ABSTRACT: All you need to start with Spark
+
+use strict;
+use warnings;
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Task::Spark::Form - All you need to start with Spark
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+perl -MCPAN -e 'install Task::Spark::Form'
+
+=head1 DESCRIPTION
+
+The bare essentials of Spark::Form, with everything
+you'll probably ever want to use to do anything with it.
+
+=head1 AUTHOR
+
+  Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Pedigree Consulting.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
